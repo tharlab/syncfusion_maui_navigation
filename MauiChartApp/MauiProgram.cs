cf42b06fc586871352078f91a80bc87d63c7f6e7
Hary AK <masterseoonline@gmail.com>
2023-05-17 17:28:05 +0700

    Add files via upload

---
+++ MauiChartApp/MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+using MauiChartApp.Data;
+using Syncfusion.Blazor;
+
+namespace MauiChartApp;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder.UseMauiApp<App>().ConfigureFonts(fonts =>{fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");});
+		builder.Services.AddMauiBlazorWebView();
+        
+	//var app = builder.Build();
+        //Register Syncfusion license
+        Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MTk5NTc4NkAzMjMxMmUzMjJlMzNtbnNUc0JIMGtWdjVpaEE2UkppelRIaWhVM2FHOG1iZG9RQVEwb2N3aStnPQ==;MTk5NTc4N0AzMjMxMmUzMjJlMzNUTFl5YjkrYUZBYWozdzZTbWc0RUNpUjEvak1LNUxqMWs3WlBTekVPM01nPQ==");
+
+        // Set IgnoreScriptIsolation as true to load scripts externally.
+        builder.Services.AddSyncfusionBlazor(options => { //options.IgnoreScriptIsolation = true; 
+		});
+
+		#if DEBUG
+        builder.Services.AddBlazorWebViewDeveloperTools();
+#endif
+		builder.Services.AddSingleton<WeatherForecastService>();
+		return builder.Build();
+	}
+}
